202f175ea90d649d2776bab4e05284391b092328
Mauro Rodrigues <mauro.developer.br@gmail.com>
2023-06-05 10:36:22 +0200

    REMOVE: REMOVED unique: true from Company

--- models/Promoter.js
+++ models/Promoter.js
@@ -9,13 +9,13 @@ const promoterSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  company: { type: String,required: true, unique: true },
+  company: { type: String,required: true},
   avatarUrl: { type: String, default: `https://firebasestorage.googleapis.com/v0/b/evento-app-5a449.appspot.com/o/default-avatar.png?alt=media&token=${IMAGE_AVATAR_DEFAULT_TOKEN}` },
   age: { type: Number},
   phone:{type: String},
   city: { type: mongoose.Schema.Types.ObjectId, ref: 'City' }, // Referência para o modelo City
   street: { type: String },
-  adrress_number: { type: String, unique: true },
+  adrress_number: { type: String },
   post_code: { type: String },
   contact: { type: String },
 });
