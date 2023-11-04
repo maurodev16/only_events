f66dd815c69d9a510d47d36bb2b14cb7ca242a3f
maurodev16 <mauro.developer@gmail.com>
2023-11-04 21:31:43 +0100

    ...

--- routes/establishmentRoutes.js
+++ routes/establishmentRoutes.js
@@ -192,8 +192,8 @@ router.post(
 function parseOpeningHours(openingHoursString) {
   const trimmedString = openingHoursString.toString().slice(1, -1); // Remova os colchetes iniciais e finais
 
-  const parts = trimmedString.toString().split("day: ");
-  parts.toString().split(", "); // Separe as partes por vírgula e espaço
+  const parts = trimmedString.toString().split("day: ,");
+
   const objects = [];
 
 
@@ -201,10 +201,10 @@ function parseOpeningHours(openingHoursString) {
 
   for (let i = 0; i < parts.length; i++) {
     const part = parts[i];
-    const openCloseParts = part.toString().split("open: ");
+    const openCloseParts = part.toString().split("open: ,");
     if (openCloseParts.length === 2) {
       const day = openCloseParts[0].trim();
-      const openClose = openCloseParts[1].toString().split("close: ");
+      const openClose = openCloseParts[1].toString().split("close: ,");
       const open = openClose[0].trim();
       const close = openClose[1].trim();
 
