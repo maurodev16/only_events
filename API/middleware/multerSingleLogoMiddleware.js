33aa01f89985522703578208d8e97f4319c0f1e5
maurodev16 <mauro.developer.br@gmail.com>
2024-02-12 12:36:34 +0100

    multer

--- API/middleware/multerSingleLogoMiddleware.js
+++ API/middleware/multerSingleLogoMiddleware.js
@@ -1,28 +1,28 @@
 import multer from 'multer';
-const storage = multer.diskStorage({
 
-    filename:function(req, file, cb){
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
 
 const fileFilter = (req, file, cb) => {
-    // Verificar se o tipo do arquivo é válido
-    const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/gif'];
-    if (allowedMimeTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        // Rejeitar o arquivo
-        cb(new Error('Unsupported Image Format, only JPEG, PNG, JPG, and GIF can be supported.'), false);
-    }
+  // Verificar se o tipo do arquivo é válido
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    // Rejeitar o arquivo
+    cb(new Error('Unsupported Image Format, only JPEG, PNG, and JPG can be supported.'), false);
+  }
 };
 
 const uploadSingleLogo = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5  // Limite de 5MB
-    },
-    fileFilter: fileFilter
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // Limite de 5MB
+  },
+  fileFilter: fileFilter,
 });
 
 export default uploadSingleLogo;
