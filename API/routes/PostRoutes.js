9881a995e15e985bfc2b24850b19943bba8c5657
maurodev16 <mauro.developer.br@gmail.com>
2024-02-20 13:17:36 +0100

    ...

--- API/routes/PostRoutes.js
+++ API/routes/PostRoutes.js
@@ -13,6 +13,7 @@ import checkToken from '../middleware/checkToken.js';
 router.post("/create-post/:establishmentObjId",muiltiImagesMiddleware.array("images"), async (req, res) => {
   try {
     const establishmentObjId = req.params.establishmentObjId;
+    const images = req.files; // Imagens enviadas na solicitação
     const postData = req.body; // Dados da postagem enviados no corpo da solicitação
 
     // Verifique se o estabelecimento existe
@@ -26,14 +27,36 @@ router.post("/create-post/:establishmentObjId",muiltiImagesMiddleware.array("ima
     if (postData.eventType && !validEventTypes.includes(postData.eventType)) {
       return res.status(400).json({ error: "Tipo de evento inválido." });
     }
+   // Check if photos for the gallery have been sent
+ // Verifique se foram enviadas imagens
+ if (!images || images.length === 0) {
+  return res.status(400).send("Nenhuma imagem fornecida");
+}
+  // Faça upload das imagens para o Cloudinary e obtenha os URLs seguros
+  const uploadedImageUrls = [];
+  for (const image of images) {
+    const result = await cloudinary.uploader.upload(image.path, {
+      resource_type: "auto",
+      allowed_formats: ["jpg", "jpeg", "png"],
+      folder: "posts",
+      overwrite: false,
+      upload_preset: "wasGehtAb_preset",
+    });
+     // Adicione os URLs das imagens à postagem
+     postData.mediaUrls = uploadedImageUrls;
 
+    if (!result.secure_url) {
+      return res.status(500).send("Erro ao fazer upload das imagens para o Cloudinary");
+    }
+    uploadedImageUrls.push(result.secure_url);
+  }
     // Crie uma nova instância de Post com os dados fornecidos
     const newPost = new Post({ ...postData, establishmentObjId });
 
     // Salve a nova postagem no banco de dados
     const savedPost = await newPost.save();
 
-    res.status(201).json(savedPost);
+    res.status(201).json({ post: newPost });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Erro ao criar a postagem." });
