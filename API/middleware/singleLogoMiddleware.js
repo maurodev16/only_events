2f8480a801476d65b0470caab29fc9a9b57857ca
maurodev16 <mauro.developer.br@gmail.com>
2024-02-22 13:06:59 +0100

    single logo updated Multer

--- API/middleware/singleLogoMiddleware.js
+++ API/middleware/singleLogoMiddleware.js
@@ -1,35 +1,35 @@
 import multer from 'multer';
 
-// Define the storage settings for multer
+// Storage 
 const storage = multer.diskStorage({
-    // Define how the filename should be stored
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "../images"));
+    },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-      console.log("file.originalname:::",file.originalname)
-
+        if (file) {
+            cb(null, new Date().toISOString().replace(/:/g, "-")
+                + file.originalname);
+        } else {
+            cb(null, false);
+        }
     }
 });
 
-// Define a function to filter the types of files allowed for upload
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
-      cb(null, true);
-      console.log("file.mimetype:::",file.mimetype)
-    } else {
-      cb({ message: 'Unsupported File Format, only JPEG, PNG end JPG can be supported.' }, false);
-    }
-  
-  };
-
-// Configure multer with the storage settings and file filter
 const singleLogoMiddleware = multer({
     storage: storage,
-    limits: {
-        // Limit the file size to 5MB
-        fileSize: 1024 * 1024 * 5 // 5 Megabytes
+    fileFilter: function (req, file, cb) {
+        if (file.mimetype.startsWith("image")) {
+            cb(null, true);
+        } else {
+            cb({ message: "Unsupported file format" }, false);
+        }
     },
-    fileFilter: fileFilter // Apply the file filter function
+    limits: { fileSize: 1024 * 1024 * 5 }, // 1 megabyte
 });
-console.log("singleLogoMiddleware:::",singleLogoMiddleware)
+
+
+console.log("singleLogoMiddleware:::", singleLogoMiddleware)
+
+
 
 export default singleLogoMiddleware; // Export the configured multer middleware
