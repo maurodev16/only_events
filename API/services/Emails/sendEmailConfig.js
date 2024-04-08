2bcf7143f6d6789749e2383962c75860e594dcdb
maurodev16 <mauro.developer.br@gmail.com>
2024-04-08 23:08:34 +0200

    ...

--- API/services/Emails/sendEmailConfig.js
+++ API/services/Emails/sendEmailConfig.js
@@ -3,18 +3,21 @@ import sgMail from '@sendgrid/mail';
 
 dotenv.config();
 
-
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmailConfig = async (receiver, subject, htmlContent) => {
     try {
+
+        // Define the email content by passing resetLink to the compiled template
+        const html = template({ resetLink });
+
         const data = {
             to: receiver,
-            from: "noreplay@wasgehtab.de",
+            from: process.env.SENDER_EMAIL, // Update with your sender email
             subject: subject,
             html: htmlContent,
         };
-
+console
         return sgMail.send(data);
     } catch (error) {
         return new Error(error);
@@ -22,4 +25,3 @@ const sendEmailConfig = async (receiver, subject, htmlContent) => {
 };
 
 export default sendEmailConfig;
-
